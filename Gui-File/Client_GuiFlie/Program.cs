5ffe2d3b589e5e0b560cf144ae2cee731f9c4c6e
luongluong <phangialuong01@gmail.com>
2018-03-11 17:04:46 +0700

    gui-file

---
+++ Gui-File/Client_GuiFlie/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Client_GuiFlie
+{
+    class Program
+    {
+        public static void CreateFIle(string pathfile, string data)
+        {
+            //E:\hoc_tap\Nam_3_HK2\Lap_trinh_mang\Server\text1.txt
+            string path = pathfile;
+            //string path = @"''";
+            if (!File.Exists(path))
+            {
+
+                try
+                // Create a file to write to.
+                {
+                    using (StreamWriter sw = File.CreateText(path))
+                    {
+                        sw.WriteLine(data);
+                    }
+                    Console.WriteLine("Thanh cong");
+                }
+                catch
+                {
+                    Console.WriteLine("Loi duong dan: ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("file da ton tai!");
+            }       
+        }
+        static void Main(string[] args)
+        {
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Loopback, 1234);
+
+            client.Connect(iPEndPoint);
+
+            string nhan = "";
+            byte[] snhan = new byte[1024];
+            int rec = client.Receive(snhan);
+            nhan = Encoding.ASCII.GetString(snhan, 0, rec);
+            Console.WriteLine(nhan);
+
+            string s;
+
+            Console.Write("Client: ");
+            s = Console.ReadLine();
+            byte[] sgui = new byte[1024];
+            sgui = Encoding.ASCII.GetBytes(s);
+            client.Send(sgui);
+
+            if (s == "fileText.txt")
+            {
+                //nhập đường đẫn
+                Console.Write("Nhap duong dan: ");
+                string sf = Console.ReadLine();
+                sf = sf + "\\" + s;
+                CreateFIle(sf, nhan);
+            }
+
+        }
+    }
+}
---
+++ Gui-File/Gui-File/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Gui_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Loopback, 1234);
+            server.Bind(iPEndPoint);
+
+            server.Listen(10);
+
+            Socket client = server.Accept();
+
+            byte[] nhan = new byte[1024];
+            int rec = client.Receive(nhan);
+            string snhan = Encoding.ASCII.GetString(nhan, 0, rec);
+
+            if(snhan == "fileText.txt")
+            {
+                FileStream fs = new FileStream("fileText.txt", FileMode.Open);
+                StreamReader rd = new StreamReader(fs, Encoding.UTF8);
+                String giatri = rd.ReadToEnd();// ReadLine() chỉ đọc 1 dòng đầu thoy, ReadToEnd là đọc hết
+                
+                byte[] gui = Encoding.ASCII.GetBytes(giatri);
+                client.Send(gui);
+
+                rd.Close();
+            }
+        }
+    }
+}
